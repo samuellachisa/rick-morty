c01066f49317da885ab3f68cce194775d7e9cc97
Samuel Lachisa <SamuelL@ethiopianairlines.com>
2023-03-17 16:20:42 +0300

    updated 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Body from './components/Body'
+import Character from './components/Character'
+import Episode from './components/Episode'
+import Location from './components/Location'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 function App() {
  
 
   return (
     <div className="App">
-      <Body/>
+      <Router>
+   
+      <Routes>
+        <Route path="/" element={<Body/>}></Route>
+        <Route path='/character' element={<Character/>} exact></Route>
+        <Route path='/episode' element={<Episode/>} exact></Route>
+        <Route path='/location' element={<Location/>} exact></Route>
+      </Routes>
+      </Router>
+    
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex place-content-center'>
+      <img src='/rickLogo.png' className='w-[20%]'/>
+    </div>
+  )
+}
+
+export default Header
